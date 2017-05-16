1cf03ebc99db68946051d486bff26696cc482f3b
fanyc <fancy9801@gmail.com>
2017-05-16 21:41:58 +0800

    后台管理系统中的用户反馈查看接口

--- mod/admin/feedbackHandler.py
+++ mod/admin/feedbackHandler.py
@@ -19,7 +19,7 @@ class FeedbackHandler(ManagerHandler):
         ask_code=self.get_argument('askcode',None)
         ret={
             'content':u'请求码错误',
-            'code':201
+            'code':400
         }
 
         if ask_code=='101':
@@ -28,10 +28,10 @@ class FeedbackHandler(ManagerHandler):
 
             try:
                 if offset<0 or limit<=0:
-                    raise ArgumentExceptions(202,u'请求参数错误')
+                    raise ArgumentExceptions(400,u'请求参数错误')
                 ret['max_page'] = int(ceil(self.db.query(FeedBack).count() / limit))
                 if offset>ret['max_page']-1:
-                    raise ArgumentExceptions(203,u'页数超出范围')
+                    raise ArgumentExceptions(400,u'页数超出范围')
                 FB=self.db.query(FeedBack).group_by(FeedBack.date.desc()).limit(limit).offset(offset*limit).all()
                 data=[]
                 for fb in FB:
@@ -45,16 +45,17 @@ class FeedbackHandler(ManagerHandler):
                     )
                     data.append(tmp)
                 ret['content']=data
+                ret['code']=200
 
             except ArgumentExceptions as e:
                 ret['content']=e.content
                 ret['code']=e.code
             except NoResultFound:
                 ret['content']=u'未找到数据'
-                ret['code']=204
+                ret['code']=500
             except Exception:
                 ret['content']=u'数据库查询出错'
-                ret['code']=205
+                ret['code']=500
         self.write(json.dumps(ret, indent=2, ensure_ascii=False))
 
     def get(self):
